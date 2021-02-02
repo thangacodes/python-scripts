0bf07d2702ec8d1f2e5d90a793a5d1307b4e0045
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-02-02 19:52:05 +0530

    Create directory-creation.py
    This is my initial commit

---
+++ directory-creation.py
@@ -0,0 +1,10 @@
+##### Create a demo directory in any of the guest os like Window or Linux ####
+
+print("PIP- Definition is 'Python installation Package'")
+###########################
+import os
+import time
+import shutil
+os.mkdir("C:/temp/demo")
+time.sleep (7)
+os.rmdir("C:/temp/demo")
