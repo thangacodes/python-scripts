b094d7d3d6bb7101a8087b95d2d907a52328ab83
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-05-11 22:05:19 +0530

    Create palindrome.py
    Initial commit

---
+++ palindrome.py
@@ -0,0 +1,7 @@
+##  How to check a string is palindrome?
+
+a=input ("enter the string value here:: ")
+if (a==a[::-1]):
+    print("Entered string is a palindrome")
+else:
+    print ("not a palindrome")
