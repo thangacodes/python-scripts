9c94ae7b51385cc9ac9c47bf8aebe8e6463804b6
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-07-28 15:32:30 +0530

    Create variables.py
    initial commit

---
+++ variables.py
@@ -0,0 +1,7 @@
+character_name = "John"
+character_age = "50"
+born_year = "1989"
+print ("There was a guy name called " +  character_name   + " , ")
+print("he is having age around " +  character_age + "," )
+print(character_name + " lives in london " + born_year + ",")
+print(character_name + " loves to play cricket and football.")
