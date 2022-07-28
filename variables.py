42c8df820cbf646100f2aecef876f726c4a071e8
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-07-28 15:46:22 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,7 +1,9 @@
 character_name = "John"
 character_age = "50"
 born_year = "1989"
+best_dish = "chicken burger"
 print ("There was a guy name called " +  character_name   + " , ")
 print("he is having age around " +  character_age + "," )
 print(character_name + " lives in london " + born_year + ",")
 print(character_name + " loves to play cricket and football.")
+print(character_name + " likes to have " + best_dish + ".")
