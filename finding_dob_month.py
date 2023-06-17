d3310ac0ef17d23a9a6aca77e2fbef84457a11de
Murugan <thangadurai.murugan@tesco.com>
2023-06-17 19:47:28 +0530

    initial commit

--- finding_dob_month.py
+++ finding_dob_month.py
@@ -1,15 +1,18 @@
 def dob_finding():
+    import time
     month_name =["January","February","March","April","May","June","July","August","September","October","November","December"]
     print (month_name)
-    month_numbers =["January","February","March","April","May","June","July","August","September","October","November","December"]
-    print(month_numbers)
+    y=len(month_name)
+    y
+    print("The length of the list is:",y)
+    print("Taking '5' seconds deep break......")
+    time.sleep(5)
     dob =input("Please enter your date of birth: ", )
     dob
     print("You've entered the dob is:", dob)
     month_value  = int(dob.split('/')[1])
-    birth_number = print("you are birth month in number is:", month_value )
-    birth_month  = print("You are born in the month of:", month_numbers[month_value -1])
+    print("you are birth month in number is:", month_value )
+    print("You are born in the month of:", (month_name[month_value -1]))
 dob_finding()
 
-
-## Takeaway is, using split function, we can split dob date into '/' and then we can use that split variable in the last. If we use minus of date of split, it fullfil our requirements.
+## Takeaway is, using split function, we can split dob date into '/' and then we can use that split variable in the last. If we use minus of date of split, it fullfil our requirements..
