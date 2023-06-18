0ddcfee26f5f8201630012b81ca2ebc1ed1f4a3a
Murugan <thangadurai.murugan@tesco.com>
2023-06-18 13:38:38 +0530

    initial commit

---
+++ split.py
@@ -0,0 +1,5 @@
+product_details = "1024-345678-09834567123"
+print("The given product_detail is:", product_details)
+print("********* Going to use split function to separate values ********")
+product_code = product_details.split('-')
+print(product_code)
