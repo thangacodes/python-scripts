ceb11c8e7e9430c813c4b8478619ee2f9ed49732
Murugan <thangadurai.murugan@tesco.com>
2023-06-19 23:43:51 +0530

    slight changes on the code

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -9,13 +9,13 @@ print("Converted height value in meter is:", meter)
 print("Calculating BMI using BMI formula...")
 bmi = weight/meter ** 2
 print("BMI value of yours is in float: ", bmi)
-print("BMI value of yours is in round off in int: ", round(bmi))
-print (" ********** REPLYING TO THE USER ABOUT THEIR BMI STATUS IN STATEMENT ********** ")
+# print("BMI value of yours is in round off in int: ", round(bmi))
+print (" ********** BMI REPORT TO THE USERS ********** ")
 if bmi <= 18.5:
     print("You're Under Weight and body in your control...")
-if bmi == 24.9:
+if bmi <= 24.9:
     print("Great! You're healthy and maintaining body correctly......")
-elif bmi == 29.9:
+elif bmi <= 29.9:
     print("You are overweight! Please take an action immediately....")
 else:
-    print("You are extra obesity!!!!...")
+    print("You are extra obesity!!!!....")
