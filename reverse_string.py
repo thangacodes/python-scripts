6020e8b7b7adb9c8f07e4b3cd2d8a014a9c33d03
Murugan <thangadurai.murugan@tesco.com>
2023-07-18 22:49:46 +0530

    initial commit

---
+++ reverse_string.py
@@ -0,0 +1,6 @@
+print("This script is to use reverse_string")
+def reverse_string(input_string):
+    return input_string[::-1]
+original_string = "Hello DevOps!"
+reversed_string = reverse_string(original_string)
+print(reversed_string)
