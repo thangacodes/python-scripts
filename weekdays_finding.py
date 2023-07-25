45544c2ea57d6b0dec8ab8934c0fe61b8c0ddefd
Murugan <thangadurai.murugan@tesco.com>
2023-07-25 22:59:53 +0530

    print information is updated

--- weekdays_finding.py
+++ weekdays_finding.py
@@ -1,11 +1,17 @@
 import time
-print("iterating weekdays using python for loop")
+print("Iterating weekdays in Python...")
 weekdays=["monday","tuesday","wednesday","thursday","friday"]
 for w in weekdays:
     print(w)
-time.sleep(3)
-print("In the above weekdays list, print only when weekday is Thursday!!")
+time.sleep(2)
+print("If the weekday is Thursday, print only when it is on the above weekday list!..")
 weekdays=["monday","tuesday","wednesday","thursday","friday"]
 for w in weekdays:
     if w == "thursday":
         print(w)
+time.sleep(2)
+weekdays = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+for w in weekdays:
+    print(w)
+    if w == "thursday":
+        break
