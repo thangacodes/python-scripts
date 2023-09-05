33ea684ec92c831d3a1e58643e07102ecb07173d
Murugan <thangadurai.murugan@tesco.com>
2023-09-05 09:40:14 +0530

    initial work

---
+++ adding_numbers_dynamically.py
@@ -0,0 +1,9 @@
+print("Program for adding two numbers dynamically:")
+a = int(input("Please enter number1: " ))
+print(a)
+b = int(input("Please enter number2: " ))
+print(b)
+print(type(a))
+print(type(b))
+c = a + b
+print("addition of two value is: ", c)
