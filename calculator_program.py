b6bda0cb692cc8c0d22cf6bfc3d7e2db773e35fc
Murugan <thangadurai.murugan@tesco.com>
2023-10-24 07:18:38 +0530

    initial work on calc program

---
+++ calculator_program.py
@@ -0,0 +1,20 @@
+num1 = int(input("Enter a number pls:"))
+print(num1)
+num2 = float(input("Enter float number pls:"))
+print(num2)
+oper=input("Enter operator(+,-,*,/,^): ")
+print(oper)
+def calculate(num1,num2,oper):
+    if oper == '+':
+        result = num1 + num2
+    elif oper == '-':
+        result = num1 - num2
+    elif oper == '*':
+        result = num1 * num2
+    elif oper == '/':
+        result = num1 / num2
+    elif oper == '^':
+        result = num1 ** num2ss
+    return result
+result=calculate(num1,num2,oper)
+print(result)
