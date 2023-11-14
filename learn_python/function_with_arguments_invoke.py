6bb17ba1708c22f67ed80ba121e531b68d4edfe2
send2durai <send2durai@hotmail.com>
2023-11-14 12:27:00 +0530

    initial work

---
+++ learn_python/function_with_arguments_invoke.py
@@ -0,0 +1,6 @@
+def birthday(name, age, place):
+    print(f"Today is {name} birthday")
+    print(f"He is {age} years old")
+    print(f"He was born in a place called {place}")
+birthday("thangadurai", 34, "chennai")
+birthday("Anvika", 1, "Bangalore")
