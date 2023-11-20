bd833b2f5978979bdaa314c1cea612e5cb8ef77a
send2durai <send2durai@hotmail.com>
2023-11-20 23:57:22 +0530

    json, requests modules imported

--- boto3_scripts/create_ssm_parameter.py
+++ boto3_scripts/create_ssm_parameter.py
@@ -3,12 +3,13 @@ import os
 import time
 import sys
 import json
+import requests
 
-param_name = "cisx.me.example.com"
+param_name = "test_string_param_create.example.com"
 region_name = "ap-south-1"
 
-print(f"String param name is: {param_name}")
-print(f"The region is: {region_name}")
+print(f"Stored param name is {param_name}")
+print(f"The region is {region_name}")
 
 ssm_client = boto3.client('ssm', region_name=region_name)
 new_string_parameter= ssm_client.put_parameter(
@@ -17,4 +18,8 @@ new_string_parameter= ssm_client.put_parameter(
         Overwrite = True,
         Tier = 'Standard',
         Value = 'login1-2')
-print(f"SSM parameter created with version is: {new_string_parameter['Version']} and Tier is: {new_string_parameter['Tier']}")
+#print(new_string_parameter)
+
+print(f"The string param Tier is: {new_string_parameter['Tier']} and the request_id is: {new_string_parameter['ResponseMetadata']['RequestId']} and http_status_code is: {new_string_parameter['ResponseMetadata']['HTTPStatusCode']} and connection_state is: {new_string_parameter['ResponseMetadata']['HTTPHeaders']['connection']}")
+
+
