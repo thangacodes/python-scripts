981e904e66ac86febc934284b1c6e57614e29989
send2durai <send2durai@hotmail.com>
2023-12-06 17:14:06 +0530

    slight changes

--- boto3_scripts/create_keypair.py
+++ boto3_scripts/create_keypair.py
@@ -1,20 +1,15 @@
 import boto3
 import os
 import time
+import boto3
 import json
 import requests
-
+  
 ## variables substituting
-keyname = 'ssh_dev.pem'
-region_name = 'ap-south-1'
-key_location = '/tmp/'
-
-## printing the variables that we substituted
-print(f"The key_name is: {keyname}")
-print(f"The region is: {region_name}")
-print(f"The keypair location is: {key_location}")
-key_path = f"{key_location}{keyname}"
-print(f"The keyfile saved location is: {key_path}")
+keyname       = "td.pem"
+region_name   = "ap-south-1"
+key_location  = "/home/ec2-user/td.pem"
+param_name    = "demo.aws.example.demo.privatekey"
 
 ## Create an EC2 client for the specified region
 ec2 = boto3.client('ec2', region_name=region_name)
@@ -23,16 +18,19 @@ ec2 = boto3.client('ec2', region_name=region_name)
 response = ec2.create_key_pair(KeyName=keyname)
 
 ## Save the private key to a file (you can change the path as needed)
-print(f"The private keypath is: {key_path}")
-with open(key_path, 'w') as key_file:
+# print(key_path)
+with open(key_location, 'w') as key_file:
     key_file.write(response['KeyMaterial'])
-
-#print(response) // To see entire JSON_PAYLOAD response
-
-print(f"The SSH_KEY is: {response['KeyName']} created.")
+time.sleep(2)
+from os import system 
+system("cat /home/ec2-user/td.pem")
+print("\n")
+print(f"The param: {param_name} to be amend with the latest keypair: {keyname} value")
+print("*************** JSON PAYLOAD RESPONSE ***************")
+print(f"The KEYPAIR_NAME is: {response['KeyName']} created.")
 print(f"The KEYPAIR_ID is: {response['KeyPairId']}.")
 print(f"The REQUEST_ID is: {response['ResponseMetadata']['RequestId']}.")
-print(f"The HTTP RESPONSE_CODE is: {response['ResponseMetadata']['HTTPStatusCode']}.")
-print(f"The HTTP REQUEST_ID is: {response['ResponseMetadata']['RequestId']}.")
-print(f"The KEYPAIR created DATE is: {response['ResponseMetadata']['HTTPHeaders']['date']}.")
-
+print(f"The CLIENT_HTTP_REQUEST_ID is: {response['ResponseMetadata']['RequestId']}.")
+print(f"The CLIENT_HTTP_RESPONSE_CODE is: {response['ResponseMetadata']['HTTPStatusCode']}.")
+print(f"The KEYPAIR is: {keyname} CREATED_DATE on: {response['ResponseMetadata']['HTTPHeaders']['date']}.")
+exit()
