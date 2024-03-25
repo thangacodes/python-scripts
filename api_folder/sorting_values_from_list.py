cca73e24e7eb0f709dd76784117781fecf929c29
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-03-25 21:00:40 +0530

    Create sorting_values_from_list.py

---
+++ api_folder/sorting_values_from_list.py
@@ -0,0 +1,14 @@
+import time
+countries = ["india", "brazil","dubai","usa","uk","india","dubai"]
+print(countries)
+if "dubai" in countries:
+    print("found china")
+else:
+    print("not found!")
+time.sleep(3)
+a = [ 1,2,3,1,5,1,2,3,4]
+print(a)
+if 1 in a:
+    print(f"the value 1 is exist")
+else:
+    print("cannot found!")
