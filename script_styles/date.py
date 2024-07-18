01a26ec0b58f8aa908e163bd7ce6ea2adf3e3570
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-07-18 17:24:13 +0530

    Update date.py

--- script_styles/date.py
+++ script_styles/date.py
@@ -1,9 +1,19 @@
 import datetime
 from rich import print
+import time
+
+name = input("What is your name:?? ")
+print(f"[italic green]My name is: {name}[/italic green] ")
+age = input("What is your age:?? ")
+print(f"[italic green] My age is: {age}[/italic green] ")
+marrige_status = input("Do you married:?? ")
+print(f"[italic green] Yes, I do {marrige_status}[/italic green] ")
+baby_status = input("Do you have kids:?? ")
+print(f"[italic green]Yes, I do have {baby_status} ")
+time.sleep(3)
 print("[italic blue]What is today's date??[/italic blue]")
-print("[italic yellow]The answer is below:[/italic yellow]")
-currentDate=datetime.datetime.today()
-date=currentDate.strftime("%d-%m-%Y")
+currentDate = datetime.datetime.today()
+date = currentDate.strftime("%m-%d-%Y")
 print(f"[italic green]Today's date, in number is (MM-DD-YYYY): {date}[/italic green]")
 
 
