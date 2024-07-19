71e3096269ea65664d8040358a32e53242a065df
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-07-19 18:31:13 +0530

    Update quiz_program.py

--- script_styles/quiz_program.py
+++ script_styles/quiz_program.py
@@ -1,38 +1,62 @@
-import datetime
-from rich import print
+import boto3
 import time
-print("[italic green] Quiz program [/italic green]")
-print("[italic blue]What is today's date in full???[/italic blue]")
+import datetime
+import rich
+from rich import print
+
+# Print the script execution date
 currentDate = datetime.datetime.today()
 date = currentDate.strftime("%m-%d-%Y")
-print(f"[italic green]Today's date, in number is (MM-DD-YYYY): {date}[/italic green]")
-time.sleep(2)
-print("[italic green]The quiz program will be starting soon! Please be patient!![/italic green]")
-name = input("What is your name:?? ")
-print(f"[italic green]My name is: {name}[/italic green] ")
-age = input("What is your age:?? ")
-print(f"[italic green] My age is: {age}[/italic green] ")
-marrige_status = input("Do you married:?? ")
-if marrige_status == "no":
-    print("[italic green] No, I'm not Married [/italic green] ")
-elif marrige_status == "yes":
-    print("[italic green] Yes, I'm Married [/italic green]")
-else:
-    print("[italic green] You entered incorrect answers [/italic green] ")
-baby_status = input("Do you have kids:?? ")
-if baby_status == "yes":
-    print("[italic green] Yes, I do have kids [/italic green] ")
-elif baby_status == "no":
-    print("[italic green] No, I do not have Kids [/italic green] ")
-else:
-    print("[italic green] You entered incorrect [/italic green] ")
-hobbies = input("What is your hobbies:?? ")
-print(f"[italic green]My hobbies are: {hobbies}[/italic green] ")
+print(f"[bold red]The Script gets executed on: {date}[/bold red]")
 
+# Variable declaration for launching EC2 machine
+AMI = "ami-0ec0e125bb6c6e8ec"
+INSTANCETYPE = "t2.micro"
+KEYPAIR = "practical"
+SGP = "sg-0fb1052b659369aa8"
+SUBNET = "subnet-01a3ea7e1f72afd59"
+USER_DATA = '''
+#!/bin/bash
+sudo yum update -y
+sudo yum install -y tree
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl status httpd 
+'''
+# Define tags correctly formatted as separate dictionaries
+tags = [
+    {'Key': 'Name', 'Value': 'Apache-Server'},
+    {'Key': 'Owner', 'Value': 'Thangadurai.murugan@example.com'},
+    {'Key': 'CreationDate', 'Value': '07/19/2024'},
+    {'Key': 'Environment', 'Value': 'Development'}
+]
 
-## Note: Firstly, you need to have these libraries from python should be installed/exist on the windows or linux OS.
-## python.exe -m pip install --upgrade pip   //upgraind the old PIP package installer from Windows OS.
-## pip install colorama ==> colorama to print text in different colors and styles
-## pip install rich  ==> rich is another Python library that provides extensive formatting options for terminal output.
+client = boto3.client('ec2')
+response = client.run_instances(
+    ImageId=AMI,
+    InstanceType=INSTANCETYPE,
+    KeyName=KEYPAIR,
+    MaxCount=1,
+    MinCount=1,
+    SecurityGroupIds=[SGP],
+    SubnetId=SUBNET,
+    UserData=USER_DATA,
+    TagSpecifications=[
+    {
+        'ResourceType': 'instance',
+        'Tags': tags
+    }
+]
+)
+time.sleep(60)
+for instance in response['Instances']:
+    print(f"[bold red]    #################################### [/bold red]")
+    print(f"[bold blue]   #################################### [/bold blue]")
+    print(f"[bold yellow] #################################### [/bold yellow]")
+    print(f"[bold green]The Instance Id is: {instance['InstanceId']}[/bold green]")
+    print(f"[bold green]The Launch Time is: {instance['LaunchTime']}[/bold green]")
+    print(f"[bold green]The Image Id is: {instance['ImageId']}[/bold green]")
+    print(f"[bold green]The Instance Type is: {instance['InstanceType']}[/bold green]")
+    print(f"[bold green]The Private IP Address is: {instance['PrivateIpAddress']}[/bold green]")
 
-
+# Please note: In order to avoid costs, I used the default private subnet in Mumbai region to create and associate the private subnet. If you want to run user-data scripts on the remote machine, it needs to be on the default public subnet or on the subnet which has the route to the Internet Gateway.
