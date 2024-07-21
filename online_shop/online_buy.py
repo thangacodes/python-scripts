f127923bb821e660463001f67c9fdcc58a16abdd
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-07-21 08:25:25 +0530

    Update and rename buy_fruits.py to online_buy.py

--- online_shop/online_buy.py
+++ online_shop/online_buy.py
@@ -2,6 +2,7 @@ import time
 import datetime
 from rich import print
 
+
 def online_shop():
     banner = """
     @@@@  Welcome To Thanga's Online Shopping World  @@@@    
@@ -11,11 +12,14 @@ def online_shop():
     available_fruits()
     script_begin()  # Call script_begin function after the banner is printed
 
+
 def available_fruits():
-    stock = ["apple", "banana", "grape", "blackberry", "blueberry", "pineapple", "strawberry", "cherry", "watermelon", "orange"]
+    stock = ["apple", "banana", "grape", "blackberry", "blueberry", "pineapple", "strawberry", "cherry", "watermelon",
+             "orange"]
     print("[bold green] Available fruits in our shop: [/bold green]")
     for fruit in stock:
         print(f"[bold green] - {fruit} [/bold green]")
+
 
 def script_begin():
     currentDate = datetime.datetime.today()
@@ -23,13 +27,15 @@ def script_begin():
     print(f"[bold blue] The Script is executed on: {date} [/bold blue]")
     print(f"[bold green] Script is used to order fruits online... [/bold green]")
 
-    fruits = ["apple", "banana", "grape", "blackberry", "blueberry", "pineapple", "strawberry", "cherry", "watermelon", "orange"]
+    fruits = ["apple", "banana", "grape", "blackberry", "blueberry", "pineapple", "strawberry", "cherry", "watermelon",
+              "orange"]
     userinput = input("Please enter the fruit that you would like to order: ").strip().lower()
 
     if userinput in fruits:
         print(f"[bold yellow] Yes, {userinput} is available and good to order it. [/bold yellow]")
     else:
-        print(f"[bold red] Sorry, {userinput} is not available in our shop. Please choose from the available fruits listed above [/bold red]")
+        print(
+            f"[bold red] Sorry, {userinput} is not available in our shop. Please choose from the available fruits listed above [/bold red]")
 
 # Call the main function to start the script
 online_shop()
