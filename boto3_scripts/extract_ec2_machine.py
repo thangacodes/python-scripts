70829daba5330bebbdee30c813318b33113fd1f6
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-07-21 20:28:51 +0530

    Update extract_ec2_machine.py

--- boto3_scripts/extract_ec2_machine.py
+++ boto3_scripts/extract_ec2_machine.py
@@ -18,12 +18,28 @@ response = client.describe_instances(
             "Values": [
                 'stopped',
             ]
-
+        },
+        {
+            "Name": "tag:Environment",
+            "Values": [
+                'Development',
+            ]
         },
     ],
 )
 
 for i in response['Reservations']:
     for instance in i['Instances']:
-        print(f"instance[InstanceId]")
-        print(f"instance[ImageId]")
+        instance_id = instance['InstanceId']
+        image_id = instance['ImageId']
+        public_ip = instance.get('PublicIpAddress')
+
+        print(f"Instance ID: {instance_id}")
+        print(f"Image ID: {image_id}")
+        print(f"Public IP Address: {public_ip}")
+
+## Please note:
+        #) With this boto3 python script, we can easily find out which instances are powered off in AWS.
+        #) To find all the EC2 instances that are powered on and running, simply modify the filter section where key value is running.
+        #) When you want to find out all the EC2 instances in a region, you can either use the instance state running or stopped in the key values map 
+        #) OR you can use tag key values like environment or costcenter.
