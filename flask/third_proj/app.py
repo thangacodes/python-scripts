71f344a7f6b5f06fceb27ceb11c9b31fbfc79262
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-08-18 11:52:15 +0530

    Update app.py

--- flask/third_proj/app.py
+++ flask/third_proj/app.py
@@ -6,5 +6,10 @@ app=Flask(__name__)
 def index():
     return render_template("index.html")
 
+# Custom 404 error page
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html'), 404
+
 if(__name__=='__main__'):
     app.run(debug=True)
