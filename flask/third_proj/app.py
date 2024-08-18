584da44bf8ec8e19af977120854fbb4f67eccfed
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-08-18 14:00:35 +0530

    port added

--- flask/third_proj/app.py
+++ flask/third_proj/app.py
@@ -10,6 +10,5 @@ def index():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('error.html'), 404
-
 if(__name__=='__main__'):
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
