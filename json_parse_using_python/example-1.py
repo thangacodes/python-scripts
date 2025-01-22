5d6f9cd355c12200eb6cedf6ac2c58036f99a818
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-01-22 10:33:43 +0530

    Update example-1.py

--- json_parse_using_python/example-1.py
+++ json_parse_using_python/example-1.py
@@ -6,4 +6,10 @@ response       = requests.get('https://jsonplaceholder.typicode.com/users/5')
 response_json  = json.loads(response.text)
 #finding_street = response_json['address']['geo']['lat']
 finding_company = response_json['company']['name']
-print(finding_company)
+print(f"Company name is: {finding_company}")
+time.sleep(2)
+finding_company = response_json['phone']
+print(f"Phone number is: {finding_company}")
+time.sleep(2)
+finding_company = response_json['username']
+print(f"User name is: {finding_company}")
