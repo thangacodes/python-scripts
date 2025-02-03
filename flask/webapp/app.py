9408a75140d06b7079849bb3c7b706bfcf5d9ece
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-02-03 11:03:34 +0530

    added timestampe

--- flask/webapp/app.py
+++ flask/webapp/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import random
+import datetime
 import logging
 app = Flask(__name__)
 # Disable Flask development server's logging (Werkzeug)
@@ -7,6 +8,7 @@ log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 @app.route('/')
 def index():
-    return render_template('index.html')
+    current_date = datetime.datetime.now().strftime ("%d-%m-%Y %H:%M:%S")
+    return render_template('index.html', current_date=current_date)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
