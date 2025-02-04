8169ea8aa156168fed08de8084bdb334d38cf702
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-02-04 10:19:40 +0530

    updating

--- flask/webapp/script_run.sh
+++ flask/webapp/script_run.sh
@@ -4,21 +4,21 @@ build_and_manage_container() {
     # variables
     IMAGE_TAG_NAME="flaskimage"
     CONT_NAME="fwebapp"
-    
+
     echo "Script runtime is:" $(date '+ %d-%m-%Y %H:%M:%S')
-    echo
     echo "Script is used to build image, run container from it.."
-    echo
     echo "Checking folder structure of the Flask Web Application.."
-    tree
+    tree $(pwd)
     sleep 2
     read -p "Please enter the image name (feed the image name in lowercase): " DIMAGE
     echo "User entered image name as:" $DIMAGE
     docker build -t $DIMAGE .
     docker images
     sleep 4
-    echo "Format of Docker Image Tag.."
+    echo "*********** Format of Docker Image Tag *****************"
     docker tag $DIMAGE:latest $IMAGE_TAG_NAME
+    echo 
+    echo "********** After tagging the Docker image, list the images again **************"
     docker images
     sleep 3
     read -p "Would you like to test the flask web application using container (select 'yes or no' in lower case): " USER_INPUT
@@ -30,9 +30,10 @@ build_and_manage_container() {
         echo
         docker run -d -p 5000:5000 --name $CONT_NAME $IMAGE_TAG_NAME
         docker ps
-        docker logs -f $CONT_NAME
+        echo 
+        echo "************** make an HTTP request ***************"
         curl -ivk http://localhost:5000/ | head -n 5
-        sleep 5
+        sleep 3
     else
         echo "User input is: $USER_INPUT, hence no progress.."
     fi
@@ -50,23 +51,21 @@ build_and_manage_container() {
     else
         echo "User input is: $USER_ACTION, hence no progress.."
     fi
-
     read -p "Would you like to docker image (say 'yes or no'): " USER_FEED
     echo "User entered input as:" $USER_FEED
     if [[ $USER_FEED == "yes" || $USER_FEED == "y" ]]; then
-        echo "You are good to remove docker image.."
-        echo "Removing docker image in progress ..."
-        echo "list out the existing docker images.."
+        echo "You are good to remove the Docker image."
+        echo "Removing the Docker image is in progress..."
+        echo "Listing the existing Docker images..."
         echo
         docker rmi -f $DIMAGE $IMAGE_TAG_NAME
         sleep 3
-        echo "finally testing if the image has deleted or not:"
+        echo "Finally, testing if the image has been deleted or not."
         docker images
     else
         echo "User input is: $USER_FEED, hence no progress.."
     fi
 }
-
-# Calling the function
+# Call the function
 build_and_manage_container
 exit 0
