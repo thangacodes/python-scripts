9e57ce3b556031e5c06c749851247d5bcb0c4bc7
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-08-30 10:13:58 +0530

    ini

---
+++ module/math_inbuilt_module.py
@@ -0,0 +1,18 @@
+import math
+
+print(f"using in-built math function..")
+
+print("Power of any number:")
+print()
+a = math.pow(2, 3)
+print(f"Two to the power of 3 is: {a}")
+
+print()
+print("Square root of any number:")
+print()
+a = math.sqrt(25)
+print(f"Square root of 25 is: {a}")
+
+print()
+print("Pi value:")
+print(math.pi)
